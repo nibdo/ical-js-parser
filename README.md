04969126aa29c3c9f3605aff9443170bb75e129f
nibdo <hello@bloben.com>
2021-01-18 02:57:37 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ical-js-parser
+# ical-js-parser [ALPHA]
+
+Simple parser from iCal string
+
+For complicated use cases use https://github.com/mozilla-comm/ical.js/
+
+## Install
+
+    npm i ical-js-parser
+
+## How to use
+
+    import ICalParser from 'ical-js-parser';
+
+    // Parse from iCal string
+
+    const result = ICalParser(iCalString);
+
+## TODO
+
+- tests
+- parsing to iCal
+- alarms
+- calendar timezones
---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+
+export interface IKeyValue {
+    key: string;
+    value: any;
+}
+
+export interface IEventParsedFromICal {
+    begin: string;
+    end: string;
+    dtstart: any;
+    dtend: any;
+    dtstamp?: string;
+    organizer?: any;
+    uid?: string;
+    attendee?: any;
+    created?: string;
+    description?: string;
+    lastModified?: string;
+    location?: string;
+    sequence?: string;
+    summary?: string;
+    transp?: string;
+    rrule?: string;
+    unknown?: string;
+}
+
+export interface IResultFromICal {
+    events: IEventParsedFromICal[];
+}
